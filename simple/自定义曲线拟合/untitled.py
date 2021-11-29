e7a5f60c2b4ddeb01db1d7bd6932b27be1fa3df8
Meidozuki <1025473107@qq.com>
2021-11-29 23:43:11 +0800

    upload

---
+++ simple/自定义曲线拟合/untitled.py
@@ -0,0 +1,30 @@
+from scipy import optimize
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Antoine方程  $lg\ p=A-{B \over T+C}$
+def func(indep_vars,A,B,C):
+    T=indep_vars
+    
+    return A-B/(T+C)
+
+#以水的饱和蒸气压为例
+t=np.array([0,10,20,30,40,50,60,70])
+p=np.array([610,1228,2338,4242,7375,12333,19910,31157])
+
+lgp=np.log10(p*(760/101e3))#转化为mmHg并取对数
+
+para_0=[10,1000,100] 
+var,co=optimize.curve_fit(func,xdata=t,ydata=lgp, max_nfev=9999,
+                                p0=para_0, #查得参数数量级，作为初始值，加快收敛
+                                bounds=(0,np.inf), #限制为正数
+                               )
+print(var)
+pred=func(t,*var)
+real=func(t,8.07,1730,233) #文献值
+
+plt.plot(t,lgp,label='measure')
+plt.plot(t,pred,label='pred')
+plt.plot(t,real,label='real')
+plt.legend()
+plt.show()
