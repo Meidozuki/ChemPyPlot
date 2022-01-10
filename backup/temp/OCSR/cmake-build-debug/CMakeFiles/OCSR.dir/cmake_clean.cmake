2daaee06cf40859cce00dd40e06c4d1dc921897e
Meidozuki <1025473107@qq.com>
2022-01-10 19:47:06 +0800

    temp

---
+++ backup/temp/OCSR/cmake-build-debug/CMakeFiles/OCSR.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OCSR.dir/main.cpp.obj"
+  "OCSR.exe"
+  "OCSR.exe.manifest"
+  "OCSR.pdb"
+  "libOCSR.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OCSR.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
